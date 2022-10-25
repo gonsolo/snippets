3072b9d4ce56bc39891c1ed496872a6c45fb8654
Gon Solo <gonsolo@gmail.com>
2022-10-25 16:55:55 +0200

    Test non-virtual.

--- main.cpp
+++ main.cpp
@@ -8,68 +8,34 @@
 
 using namespace std;
 
-void cpp11() {
-        auto a = {1, 2, 3};
-        for (auto& i : a ) {
-                cout << i << endl;
-        }
-}
+class Base {
 
-auto cpp14() {
-        return 134'566'000;
-}
+protected:
 
-ostream& operator<< (ostream& os, byte b) {
-        auto i = std::to_integer<int>(b);
-        auto bits = std::bitset<8>(i);
-        return os << i << " " << bits;
-}
+  void a() {
+    b(); 
+  }
 
-void cpp17() {
-        byte b{0x2A};
-        cout << b << endl;
-}
-
-template<typename T>
-concept Hashable = requires(T a) {
-        { std::hash<T>{}(a) } -> std::convertible_to<std::size_t>;
+  void b() {
+    cout << "Base b" << endl; 
+  }
 };
 
-template<Hashable T>
-void f(T i) {
-        cout << i << " " << hash<std::string>{}(i) << endl;
-}
+class Derived: public Base {
 
-void cpp20() {
-        f("42"s);
-}
+public:
 
-consteval int add(int a, int b) {
-                return a + b;
-}
+  void a() {
+    Base::a(); 
+  }
 
-constexpr int add_or_mul(int a, int b) {
-        if consteval {
-                return add(a, b);
-        } else {
-                return a * b;
-        }
-}
+  void b() {
+    cout << "Derived b" << endl; 
+  }
+};
 
+int main() {
 
-void cpp23() {
-        cout << 15uz << endl;
-
-        const int i = 3;
-        const int j = 3;
-        static_assert(add_or_mul(i, j) == 6);
-        cout << add_or_mul(i, j) << endl;
-}
-
-int main(){
-        cpp11();
-        cout << cpp14() << endl;
-        cpp17();
-        cpp20();
-        cpp23();
+  Derived* d = new Derived();
+  d->a();
 }
