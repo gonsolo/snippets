3e607aa6eae20f5fc67d9977a5368fe5a3ead0b9
Gon Solo <gonsolo@gmail.com>
2023-01-26 15:44:26 +0100

    Successful texture lookup.

--- main.cpp
+++ main.cpp
@@ -8,5 +8,19 @@ using namespace std;
 int main()
 {
 	auto textureSytem = OIIO::TextureSystem::create();
+
+	OIIO::ustring filename("Skydome.pfm");
+	OIIO::TextureOpt options;
+	float s = 0.5;
+	float t = 0.5;
+	float dsdx = 0;
+	float dtdx = 0;
+	float dsdy = 0;
+	float dtdy = 0;
+	int nchannels = 3;
+	float result[3];
+	auto successful = textureSytem->texture(filename, options, s, t, dsdx, dtdx, dsdy, dtdy, nchannels, result);
+	cout << result[0] << " " << result[1] << " " << result[2] << endl;
+	OIIO::TextureSystem::destroy(textureSytem);
 }
 
