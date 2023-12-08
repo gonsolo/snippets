5aacfe062aac9dde722161e1e6562dcfecdface7
Gon Solo <gonsolo@gmail.com>
2023-12-08 12:02:41 +0100

    Input and result as hex.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <bitset>
 #include <cstdint>
-#include <cstring>
 #include <iostream>
 #include <numbers>
 
@@ -12,9 +11,21 @@ auto main() -> int
 		float f;
 		uint32_t i;
 	} uf;
-	uf.f = numbers::inv_pi;
-	uint32_t u;
-	memcpy(&u, &uf.f, sizeof(u));
-	cout << std::bitset<32>(u) << endl;
+
+	float input = -33.f;
+	uf.f = input;
+	cout << "Input: " << input << endl;
+	cout << std::bitset<32>(uf.i) << endl;
+	cout << "32 bit float input " << input << " as uint32: dec: " << uf.i << ", hex: " << hex << uf.i << endl;
+
+	float pi = numbers::pi_v<float>;
+	float result = input / pi;
+	uf.f = result;
+
+	uint8_t i8 = uf.i;
+	cout << std::bitset<32>(uf.i) << endl;
+	cout << "                        " << std::bitset<8>(i8) << endl;
+	cout << "32 bit float result " << result << " as uint32: dec: " << uf.i << ", hex: " << hex << uf.i << endl;
+	cout << "The lowest 8 bits of 1.f / pi as uint8: " << hex << (int)i8 << endl;
 }
 
